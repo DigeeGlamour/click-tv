2e15c2a12bc5f59f71868d6199e40eed9c25f9ab
DigeeGlamour <122990259+DigeeGlamour@users.noreply.github.com>
2026-08-12 11:47:10 +0600

    Add files via upload

--- site/sw.js
+++ site/sw.js
@@ -3,7 +3,7 @@
  * Live stream, video segment ও playback proxy request স্পর্শ করবে না।
  */
 
-const CACHE_VERSION = "click-tv-design-playback-20260812-v16";
+const CACHE_VERSION = "click-tv-design-playback-20260812-v17";
 const APP_CACHE = `${CACHE_VERSION}-app`;
 const DATA_CACHE = `${CACHE_VERSION}-data`;
 
